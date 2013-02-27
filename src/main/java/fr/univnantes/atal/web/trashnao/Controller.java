bb628d7b76759390c542a6437c9d06c8d9a44fa4
Mog <mogzor@gmail.com>
2013-02-27 23:38:37 +0100

    add a basic example of how to retrieve the dataset from the NOD's site and some basic parsing to check data consistency

--- src/main/java/fr/univnantes/atal/web/trashnao/Controller.java
+++ src/main/java/fr/univnantes/atal/web/trashnao/Controller.java
@@ -6,12 +6,10 @@ package fr.univnantes.atal.web.trashnao;
 
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -31,13 +29,15 @@ public class Controller extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String path = request.getPathInfo();
+        System.out.println(path);
         RequestDispatcher rd;
-        if (false) {
-            
+        if (path.equals("/Fetch")) {
+            rd = getServletContext().getNamedDispatcher("Fetch");
         } else {
             rd = getServletContext().getNamedDispatcher("Index");
-            rd.forward(request, response);
         }
+        rd.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">

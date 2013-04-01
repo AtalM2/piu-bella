f8de3a8627d70f79826c9bb6386c063d78029b28
Mog <mogzor@gmail.com>
2013-04-01 11:02:40 +0200

    fix encoding

--- src/main/java/fr/univnantes/atal/web/trashnao/servlets/FetchJson.java
+++ src/main/java/fr/univnantes/atal/web/trashnao/servlets/FetchJson.java
@@ -1,28 +1,18 @@
 package fr.univnantes.atal.web.trashnao.servlets;
 
-import au.com.bytecode.opencsv.CSVReader;
-import fr.univnantes.atal.web.trashnao.model.Address;
-import fr.univnantes.atal.web.trashnao.model.CollectDay;
-import fr.univnantes.atal.web.trashnao.persistence.PMF;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
-import javax.jdo.PersistenceManager;
-import javax.jdo.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.net.URLConnection;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 
 public class FetchJson extends HttpServlet {
 
     //PersistenceManager pm = PMF.get().getPersistenceManager();
-
     /**
      * Handles the HTTP
      * <code>GET</code> method.
@@ -35,21 +25,18 @@ public class FetchJson extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //System.setProperty("http.proxyHost", "cache.wifi.univ-nantes.fr");
-        //System.setProperty("http.proxyPort", "3128");
         URL url = new URL("http://data.nantes.fr/api/publication/"
                 + "JOURS_COLLECTE_DECHETS_VDN/JOURS_COLLECTE_DECHETS_VDN_STBL/"
                 + "content/?format=json");
-        //Collection<Address> addresses = new ArrayList<>();
-        URLConnection yc = url.openConnection();
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-                yc.getInputStream()));
-        String inputLine;
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter pw = response.getWriter();
-        while ((inputLine = in.readLine()) != null)
-             pw.println(inputLine);
-        in.close();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(
+                     url.openStream(), "UTF-8"))) {
+            String inputLine;
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter pw = response.getWriter();
+            while ((inputLine = in.readLine()) != null) {
+                pw.println(inputLine);
+            }
+        }
     }
 }

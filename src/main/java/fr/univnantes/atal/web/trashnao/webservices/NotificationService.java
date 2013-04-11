e3fd6b959d5fab4d524c2af55d955f91b6a59151
Mog <mogzor@gmail.com>
2013-04-11 12:28:34 +0200

    going on with authentication and notifs

--- src/main/java/fr/univnantes/atal/web/trashnao/webservices/NotificationService.java
+++ src/main/java/fr/univnantes/atal/web/trashnao/webservices/NotificationService.java
@@ -1,147 +1,127 @@
 package fr.univnantes.atal.web.trashnao.webservices;
 
+import fr.univnantes.atal.web.trashnao.model.Address;
 import fr.univnantes.atal.web.trashnao.model.Notification;
 import fr.univnantes.atal.web.trashnao.model.NotificationTransport;
 import fr.univnantes.atal.web.trashnao.model.User;
-import fr.univnantes.atal.web.trashnao.security.TokenVerifier;
+import fr.univnantes.atal.web.trashnao.persistence.PMF;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class NotificationService extends WebService {
+public class NotificationService extends AuthWebService {
 
     @Override
-    protected void get(
+    protected void auth_get(
             HttpServletRequest request,
-            HttpServletResponse response)
+            HttpServletResponse response,
+            User user)
             throws ServletException, IOException {
-        User user = TokenVerifier.getUser(request);
-        if (user != null) {
-            Map<String, Object> data = new HashMap<>();
-            Set<Notification> notifications = user.getNotifications();
-            data.put("status", "success");
-            data.put("data", notifications);
+        Map<String, Object> data = new HashMap<>();
+        Set<Notification> notifications = user.getNotifications();
+        data.put("status", "success");
+        data.put("data", notifications);
 
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
-            try (PrintWriter out = response.getWriter()) {
-                out.println(mapper.writeValueAsString(data));
-            }
-        } else {
-            error(request,
-                    response,
-                    "Couldn't identify the user thanks to the access_token.",
-                    HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println(mapper.writeValueAsString(data));
         }
     }
 
     @Override
-    protected void post(
+    protected void auth_post(
             HttpServletRequest request,
-            HttpServletResponse response)
+            HttpServletResponse response,
+            User user)
             throws ServletException, IOException {
         String json = request.getParameter("data");
         if (json != null) {
             Map<String, Object> data = mapper.readValue(json, Map.class);
-            String street;
             List<String> yellowRaw, blueRaw;
+            Address address = null;
             Set<NotificationTransport> yellow = new HashSet<>(),
                     blue = new HashSet<>();
             if (data != null) {
-                if (data.containsKey("street")) {
-                    street = (String) data.get("street");
-                } else {
-                    error(request,
-                            response,
-                            "The street field must be present and set "
-                            + "to a valid street contained in the NOD "
-                            + "dataset.",
-                            HttpServletResponse.SC_BAD_REQUEST);
-                    return;
-
-                }
-                if (data.containsKey("yellow")) {
-                    yellowRaw = (List<String>) data.get("yellow");
-                    for (String transport : yellowRaw) {
+                PersistenceManager pm = PMF.get().getPersistenceManager();
+                try {
+                    if (data.containsKey("street")) {
+                        String street = (String) data.get("street");
+                        Query q = pm.newQuery(Address.class);
+                        q.setFilter("street == '" + street + "'");
                         try {
-                            yellow.add(NotificationTransport.valueOf(transport));
-                        } catch (IllegalArgumentException e) {
-                            error(request,
-                                    response,
-                                    "The yellow field isn't correct. Should be "
-                                    + "of the form [\"XMPP\", \"EMAIL\"].",
-                                    HttpServletResponse.SC_BAD_REQUEST);
-                            return;
+                            List<Address> results =
+                                    (List<Address>) q.execute();
+                            if (!results.isEmpty()) {
+                                address = results.get(0);
+                            } else {
+                            }
+                        } finally {
+                            q.closeAll();
                         }
                     }
-                    if (data.containsKey("blue")) {
-                        blueRaw = (List<String>) data.get("blue");
-                        for (String transport : blueRaw) {
+                    if (address == null) {
+                        error(request,
+                                response,
+                                "The street field must be present and set "
+                                + "to a valid street contained in the NOD "
+                                + "dataset.",
+                                HttpServletResponse.SC_BAD_REQUEST);
+                        return;
+
+                    }
+                    if (data.containsKey("yellow")) {
+                        yellowRaw = (List<String>) data.get("yellow");
+                        for (String transport : yellowRaw) {
                             try {
-                                blue.add(NotificationTransport.valueOf(transport));
+                                yellow.add(NotificationTransport.valueOf(transport));
                             } catch (IllegalArgumentException e) {
                                 error(request,
                                         response,
-                                        "The blue field isn't correct. Should be "
+                                        "The yellow field isn't correct. Should be "
                                         + "of the form [\"XMPP\", \"EMAIL\"].",
                                         HttpServletResponse.SC_BAD_REQUEST);
                                 return;
                             }
                         }
+                        if (data.containsKey("blue")) {
+                            blueRaw = (List<String>) data.get("blue");
+                            for (String transport : blueRaw) {
+                                try {
+                                    blue.add(NotificationTransport.valueOf(transport));
+                                } catch (IllegalArgumentException e) {
+                                    error(request,
+                                            response,
+                                            "The blue field isn't correct. Should be "
+                                            + "of the form [\"XMPP\", \"EMAIL\"].",
+                                            HttpServletResponse.SC_BAD_REQUEST);
+                                    return;
+                                }
+                            }
+                        }
                     }
+                    Notification notification = new Notification(address);
+                    for (NotificationTransport transport : yellow) {
+                        notification.addNotificationOnYellowDay(transport);
+                    }
+                    for (NotificationTransport transport : blue) {
+                        notification.addNotificationOnBlueDay(transport);
+                    }
+                    user.addNotification(notification);
+                    pm.makePersistent(user);
+
+                } finally {
+                    pm.close();
                 }
-                try (PrintWriter out = response.getWriter()) {
-                    out.println(data);
-                    out.println(yellow);
-                    out.println(street);
-                    out.println(blue);
-                }
-//            User user = TokenVerifier.getUser(request);
-//            if (user != null) {
-//                PersistenceManager pm = PMF.get().getPersistenceManager();
-//                try {
-//                    Address address = null;
-//                    Query q = pm.newQuery(Address.class);
-//                    q.setFilter("street == 'Allée Alain Gerbault'");
-//                    try {
-//                        List<Address> results =
-//                                (List<Address>) q.execute();
-//                        if (!results.isEmpty()) {
-//                            address = results.get(0);
-//                        } else {
-//                        }
-//                    } finally {
-//                        q.closeAll();
-//                    }
-//
-//                    if (address != null) {
-//                        Notification notification = new Notification();
-//                        notification.addNotificationOnBlueDay(NotificationTransport.XMPP);
-//                        notification.addNotificationOnYellowDay(NotificationTransport.EMAIL);
-//                        notification.setAddress(address);
-//                        user.addNotification(notification);
-//                        pm.makePersistent(user);
-//                        try (PrintWriter out = response.getWriter()) {
-//                            out.println("Email: " + user.getEmail());
-//                            out.println("Address: " + address.getStreet());
-//                        }
-//                    } else {
-//                        try (PrintWriter out = response.getWriter()) {
-//                            out.println("address error.");
-//                        }
-//                    }
-//                } finally {
-//                    pm.close();
-//                }
-//            }
             }
         }
     }
--- src/main/webapp/js/piubella.js
+++ src/main/webapp/js/piubella.js
@@ -1,35 +1,44 @@
-var PiuBella = function(conf) { };
+var PiuBella = function(conf) {
+};
 
 /**
  */
 PiuBella.prototype.loadNotifications = function(selector) {
-  $.getJSON("/notification",function(data){
-    $(selector).html(
-      $("#notifications-options").render(data.data)
-    );
-  })
+    $.getJSON("/notification",function(data){
+        $(selector).html(
+            $("#notifications-options").render(data.data)
+            );
+    })
 };
 
 /**
  */
 PiuBella.prototype.addNotification = function(street, yellow, blue) {
-  $.ajax('/notification', {
-    type: 'POST',
-    data: {data: JSON.stringify({
-            street: street,
-            yellow: yellow,
-            blue: blue
-        })},
-    success: function() { /* notif ajoutée */ }
-  });
+    $.ajax('/notification', {
+        type: 'POST',
+        data: {
+            data: JSON.stringify({
+                access_token: gapi.auth.getToken(),
+                street: street,
+                yellow: yellow,
+                blue: blue
+            })
+        },
+        success: function(data) {
+            console.log(data);
+        }
+    });
 };
 
 /**
  */
 PiuBella.prototype.deleteNotification = function(id) {
-  $.ajax('/notifications', {
-    method: 'delete',
-    data: {   },
-    success: function() { /* notif supprimée */ }
-  });
+    $.ajax('/notifications', {
+        method: 'delete',
+        data: JSON.stringify({
+            method: 'delete',
+            access_token: gapi.auth.getToken()
+        }),
+        success: function() { /* notif supprimée */ }
+    });
 };
